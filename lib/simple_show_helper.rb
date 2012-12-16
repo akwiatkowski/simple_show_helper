703dc361710cd182ef85faacd2d2198e5e7f6722
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-12-16 17:30:30 +0100

    refactoring

--- lib/simple_show_helper.rb
+++ lib/simple_show_helper.rb
@@ -1,46 +1,51 @@
-module ApplicationHelper
-# Usage:
-# use in view with bootstrap
-#   = simple_show_helper(User.first)
-#
-# Keep in mind:
-# * it uses only locales in format like this 'en.user.email'
-#
-# Custom attributes:
-#   = simple_show_helper(User.first, attrs: ["email", "name"])
-# Add created_at, updated_at:
-#   = simple_show_helper(User.first, timestamps: true)
-# Custom table class
-#   = simple_show_helper(User.first, table_class: "table")
-#
-# Example:
-#= raw simple_show_helper(resource, attrs: ["name"], timestamps: true, table_class: "table")
+class SimpleShowHelper
+  def humanize(o)
+    return case value.class.to_s
+             when "Time", "ActiveSupport::TimeWithZone" then
+               l(value, format: :long)
+             when "TrueClass" then
+               I18n.t(:true)
+             when "FalseClass" then
+               I18n.t(:no)
+             else
+               value
+           end
 
-  def simple_show_helper(m, options = { })
-    if options[:table_class]
-      table_class = options[:table_class]
-    else
-      table_class = "table table-striped table-bordered"
+  end
+
+  def render_table(data, table_class)
+    s = "<table class=\"#{table_class}\">"
+    data.each do |dat|
+      s += "<tr>\n"
+      dat.each do |d|
+        s += "<td>#{d}</td>\n"
+      end
+      s += "</tr>\n"
     end
+    s += "</table>"
 
+    return s
+  end
+
+  def scan_object
     # column used to describe association
     association_desc_keys = ["name", "email", "desc", "type", "id"]
     special_attrs = ["id", "created_at", "updated_at"]
-    object_tableize = m.class.to_s.tableize.singularize
+    object_tableize = @object.class.to_s.tableize.singularize
 
     # foreign keys, beware! magic!
-    assoc_keys = m.class.reflect_on_all_associations.collect { |a| a.foreign_key }.uniq & m.attributes.keys
+    assoc_keys = @object.class.reflect_on_all_associations.collect { |a| a.foreign_key }.uniq & @object.attributes.keys
     # associations classes
     associations = Hash.new
     assoc_keys.each do |ak|
-      associations[ak] = m.class.reflect_on_all_associations.select { |a| a.foreign_key == ak }.first.klass
+      associations[ak] = @object.class.reflect_on_all_associations.select { |a| a.foreign_key == ak }.first.klass
     end
 
-    if options[:attrs]
-      attrs = options[:attrs].collect { |k| k.to_s }
+    if @options[:attrs]
+      attrs = @options[:attrs].collect { |k| k.to_s }
     else
       # remove special attributes
-      attrs = m.attributes.keys.sort
+      attrs = @object.attributes.keys.sort
       special_attrs.each do |s|
         attrs.delete(s)
       end
@@ -50,19 +55,9 @@ module ApplicationHelper
     details = Array.new
     attrs.each do |d|
       # key = I18n.t("#{object_tableize}.#{d}")
-      key = m.class.human_attribute_name(d)
-      value = m.attributes[d]
+      key = @object.class.human_attribute_name(d)
+      value = @object.attributes[d]
 
-      value = case value.class.to_s
-                when "Time", "ActiveSupport::TimeWithZone" then
-                  l(value, format: :long)
-                when "TrueClass" then
-                  I18n.t(:true)
-                when "FalseClass" then
-                  I18n.t(:no)
-                else
-                  value
-              end
 
       if assoc_keys.include?(d)
         # nobody wants id
@@ -94,27 +89,51 @@ module ApplicationHelper
     end
 
     # timestamps
-    if options[:timestamps]
+    if @options[:timestamps]
       #details << [m.class.human_attribute_name("created_at"), l(m.attributes["created_at"], format: :long)]
       #details << [m.class.human_attribute_name("updated_at"), l(m.attributes["updated_at"], format: :long)]
       details << [I18n.t("created_at"), l(m.attributes["created_at"], format: :long)]
       details << [I18n.t("updated_at"), l(m.attributes["updated_at"], format: :long)]
     end
 
-    s = simple_show_helper_render_table(details, table_class)
-
-    return s
+    @details = details
   end
 
-  def simple_show_helper_render_table(data, table_class)
-    s = "<table class=\"#{table_class}\">"
-    data.each do |dat|
-      s += "<tr>\n"
-      dat.each do |d|
-        s += "<td>#{d}</td>\n"
-      end
-      s += "</tr>\n"
-    end
-    s += "</table>"
+  def initialize(_object, _options)
+    @object = _object
+    @options = _options
+    @table_class = _options[:table_class] || "table table-striped table-bordered"
+
+    scan_object
   end
+
+  def to_s
+    @s = render_table(@details, @table_class)
+    return @s
+  end
+end
+
+module ApplicationHelper
+# Usage:
+# use in view with bootstrap
+#   = simple_show_helper(User.first)
+#
+# Keep in mind:
+# * it uses only locales in format like this 'en.user.email'
+#
+# Custom attributes:
+#   = simple_show_helper(User.first, attrs: ["email", "name"])
+# Add created_at, updated_at:
+#   = simple_show_helper(User.first, timestamps: true)
+# Custom table class
+#   = simple_show_helper(User.first, table_class: "table")
+#
+# Example:
+#= raw simple_show_helper(resource, attrs: ["name"], timestamps: true, table_class: "table")
+
+  def simple_show_helper(m, options = { })
+    return SimpleShowHelper.new(m, options).to_s
+  end
+
+
 end
